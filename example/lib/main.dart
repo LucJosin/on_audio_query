b1a34b1e047c0a442a2b0967446a845da898e85e
Lucas Pinheiro <lucaspinheiro.lp@hotmail.com>
2021-03-27 20:45:55 -0300

    Fixed flutter example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -7,6 +7,8 @@ Copyright: © 2021, Lucas Josino. All rights reserved.
 License: https://github.com/LucasPJS/on_audio_query/blob/main/LICENSE
 */
 
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:on_audio_query/on_audio_query.dart';
 
@@ -35,47 +37,48 @@ class _SongsState extends State<Songs> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Songs"),
-        elevation: 2,
-      ),
-      body: FutureBuilder(
-        future: OnAudioQuery()
-            .querySongs(SongSortType.DEFAULT, OrderType.ASC_OR_SMALLER, true),
-        builder: (context, item) {
-          if (item.data != null) {
-            songList = item.data;
-            return ListView.builder(
-              itemCount: songList.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  onTap: () => songInfo(context, index),
-                  title: Text(songList[index].title),
-                  subtitle: Text(songList[index].artist),
-                  trailing: Icon(Icons.arrow_forward_rounded),
-                  leading: version >= 29
-                      ? QueryArtworkWidget(
-                          id: int.parse(songList[index].id),
-                          type: ArtworkType.AUDIO,
-                        )
-                      : ClipRRect(
-                          borderRadius: BorderRadius.circular(50),
-                          child: Image(
-                            image: FileImage(
-                              File(songList[index].artwork),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Songs"),
+          elevation: 2,
+        ),
+        body: FutureBuilder(
+          future: OnAudioQuery()
+              .querySongs(SongSortType.DEFAULT, OrderType.ASC_OR_SMALLER, true),
+          builder: (context, item) {
+            if (item.data != null) {
+              songList = item.data;
+              return ListView.builder(
+                itemCount: songList.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(songList[index].title),
+                    subtitle: Text(songList[index].artist),
+                    trailing: Icon(Icons.arrow_forward_rounded),
+                    leading: version >= 29
+                        ? QueryArtworkWidget(
+                            id: songList[index].id,
+                            type: ArtworkType.AUDIO,
+                          )
+                        : ClipRRect(
+                            borderRadius: BorderRadius.circular(50),
+                            child: Image(
+                              image: FileImage(
+                                File(songList[index].artwork),
+                              ),
+                              height: 50,
+                              width: 50,
+                              fit: BoxFit.cover,
                             ),
-                            height: 50,
-                            width: 50,
-                            fit: BoxFit.cover,
                           ),
-                        ),
-                );
-              },
-            );
-          }
-          return CircularProgressIndicator();
-        },
+                  );
+                },
+              );
+            }
+            return CircularProgressIndicator();
+          },
+        ),
       ),
     );
   }
