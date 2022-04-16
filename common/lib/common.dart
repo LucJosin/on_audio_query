afaf5e903efa5ac71a14e58203bd37789f40c9da
Lucas Pinheiro <contact@lucasjosino.com>
2022-04-15 22:27:38 -0300

    Converted `common` from `dart package` to `flutter package`

--- common/lib/common.dart
+++ common/lib/common.dart
@@ -1,10 +1,1 @@
-import 'package:common/src/methods/queries/audios_query.dart';
-import 'package:on_audio_query_platform_interface/on_audio_query_platform_interface.dart';
-
-class FakeOnAudioQuery {
-  Future<List<AudioModel>> queryAudios(List files) async {
-    return AudiosQuery().queryAudios(
-      filter: MediaFilter.forAudios(),
-    );
-  }
-}
+class FakeOnAudioQuery {}
