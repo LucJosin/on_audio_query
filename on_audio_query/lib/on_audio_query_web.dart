2aeab3615c95c4c7986af1f12dbd82c0b6d8298d
Lucas Pinheiro <contact@lucasjosino.com>
2022-04-22 13:43:29 -0300

    Re-added `web` methods

--- on_audio_query/lib/on_audio_query_web.dart
+++ on_audio_query/lib/on_audio_query_web.dart
@@ -13,18 +13,13 @@ Copyright: © 2021, Lucas Josino. All rights reserved.
 =============
 */
 
-// import 'dart:async';
-// import 'dart:typed_data';
-
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:on_audio_query_platform_interface/on_audio_query_platform_interface.dart';
 
-// import 'src/methods/helper/query_helper.dart';
-// import 'src/methods/queries/songs_query.dart';
-// import 'src/methods/queries/albums_query.dart';
-// import 'src/methods/queries/artists_query.dart';
-// import 'src/methods/queries/genres_query.dart';
-// import 'src/methods/queries/artwork_query.dart';
+import 'src/queries/albums_query.dart';
+import 'src/queries/artists_query.dart';
+import 'src/queries/audios_query.dart';
+import 'src/queries/genres_query.dart';
 
 /// A web implementation of the OnAudioQueryWeb plugin.
 class OnAudioQueryPlugin extends OnAudioQueryPlatform {
@@ -33,90 +28,61 @@ class OnAudioQueryPlugin extends OnAudioQueryPlatform {
     OnAudioQueryPlatform.instance = OnAudioQueryPlugin();
   }
 
-  // // Helper
-  // final QueryHelper _helper = QueryHelper();
-
-  // // Query methods
-  // final SongsQuery _songsQuery = SongsQuery();
-  // final AlbumsQuery _albumsQuery = AlbumsQuery();
-  // final ArtistsQuery _artistsQuery = ArtistsQuery();
-  // final GenresQuery _genresQuery = GenresQuery();
+  // Query methods
+  final AudiosQuery _audiosQuery = AudiosQuery();
+  final AlbumsQuery _albumsQuery = AlbumsQuery();
+  final ArtistsQuery _artistsQuery = ArtistsQuery();
+  final GenresQuery _genresQuery = GenresQuery();
   // final ArtworkQuery _artworkQuery = ArtworkQuery();
 
-  // @override
-  // Future<List<AudioModel>> querySongs({
-  //   MediaFilter? filter,
-  //   SongSortType? sortType,
-  //   OrderType? orderType,
-  //   UriType? uriType,
-  //   bool? ignoreCase,
-  //   String? path,
-  // }) async {
-  //   // TODO: Fix web platform..
-  //   return _songsQuery.querySongs(sortType, orderType, ignoreCase!, path);
-  // }
+  @override
+  Future<List<AudioModel>> queryAudios({
+    MediaFilter? filter,
+    bool? isAsset = true,
+    SongSortType? sortType,
+    OrderType? orderType,
+    UriType? uriType,
+    bool? ignoreCase,
+    String? path,
+  }) async {
+    return _audiosQuery.queryAudios(filter: filter);
+  }
 
-  // @override
-  // Future<List<AlbumModel>> queryAlbums({
-  //   MediaFilter? filter,
-  //   AlbumSortType? sortType,
-  //   OrderType? orderType,
-  //   UriType? uriType,
-  //   bool? ignoreCase,
-  // }) async {
-  //   return _albumsQuery.queryAlbums(sortType, orderType, ignoreCase!);
-  // }
+  @override
+  Future<List<AlbumModel>> queryAlbums({
+    MediaFilter? filter,
+    bool? isAsset = true,
+    AlbumSortType? sortType,
+    OrderType? orderType,
+    UriType? uriType,
+    bool? ignoreCase,
+  }) async {
+    return _albumsQuery.queryAlbums(filter: filter);
+  }
 
-  // @override
-  // Future<List<ArtistModel>> queryArtists({
-  //   MediaFilter? filter,
-  //   ArtistSortType? sortType,
-  //   OrderType? orderType,
-  //   UriType? uriType,
-  //   bool? ignoreCase,
-  // }) async {
-  //   return _artistsQuery.queryArtists(sortType, orderType, ignoreCase!);
-  // }
+  @override
+  Future<List<ArtistModel>> queryArtists({
+    MediaFilter? filter,
+    bool? isAsset = true,
+    ArtistSortType? sortType,
+    OrderType? orderType,
+    UriType? uriType,
+    bool? ignoreCase,
+  }) async {
+    return _artistsQuery.queryArtists(filter: filter);
+  }
 
-  // @override
-  // Future<List<GenreModel>> queryGenres({
-  //   MediaFilter? filter,
-  //   GenreSortType? sortType,
-  //   OrderType? orderType,
-  //   UriType? uriType,
-  //   bool? ignoreCase,
-  // }) async {
-  //   return _genresQuery.queryGenres(sortType, orderType, ignoreCase!);
-  // }
-
-  // @override
-  // Future<List<AudioModel>> queryAudiosFrom(
-  //   AudiosFromType type,
-  //   Object where, {
-  //   SongSortType? sortType,
-  //   OrderType? orderType,
-  //   bool? ignoreCase,
-  // }) async {
-  //   return [];
-  // }
-
-  // @override
-  // Future<List<dynamic>> queryWithFilters(
-  //   String argsVal,
-  //   WithFiltersType withType,
-  //   dynamic args,
-  // ) async {
-  //   return [];
-  // }
-
-  // @override
-  // Future<DeviceModel> queryDeviceInfo() async {
-  //   return DeviceModel({
-  //     "device_model": _helper.parseUserAgentToBrowserName(),
-  //     "device_sys_type": "Web",
-  //     "device_sys_version": -1,
-  //   });
-  // }
+  @override
+  Future<List<GenreModel>> queryGenres({
+    MediaFilter? filter,
+    bool? isAsset = true,
+    GenreSortType? sortType,
+    OrderType? orderType,
+    UriType? uriType,
+    bool? ignoreCase,
+  }) async {
+    return _genresQuery.queryGenres(filter: filter);
+  }
 
   // @override
   // Future<Uint8List?> queryArtwork(
