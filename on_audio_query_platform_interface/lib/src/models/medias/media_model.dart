615e3da233aa052bc0b13734a6ac19191b09c09a
Lucas Pinheiro <contact@lucasjosino.com>
2022-05-17 09:55:06 -0300

    Added more documentation

--- on_audio_query_platform_interface/lib/src/models/medias/media_model.dart
+++ on_audio_query_platform_interface/lib/src/models/medias/media_model.dart
@@ -1,8 +1,8 @@
 part of models_controller;
 
-///
+/// The default implementation/interface for all 'medias'.
 abstract class MediaModel {
-  ///
+  /// The default implementation/interface for all 'medias'.
   MediaModel(this.id);
 
   /// Return the media [id].
