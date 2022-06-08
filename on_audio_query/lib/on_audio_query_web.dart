208e98fd89e1fc4fe6bf889b824f90a6f67c1468
Lucas Josino <contact@lucasjosino.com>
2022-06-08 20:41:40 -0300

    Define `web` as `unimplemented`

--- on_audio_query/lib/on_audio_query_web.dart
+++ on_audio_query/lib/on_audio_query_web.dart
@@ -16,11 +16,6 @@ Copyright: © 2021, Lucas Josino. All rights reserved.
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:on_audio_query_platform_interface/on_audio_query_platform_interface.dart';
 
-// import 'src/methods/queries/albums_query.dart';
-// import 'src/methods/queries/artists_query.dart';
-// import 'src/methods/queries/audios_query.dart';
-// import 'src/methods/queries/genres_query.dart';
-
 /// A web implementation of the OnAudioQueryWeb plugin.
 class OnAudioQueryPlugin extends OnAudioQueryPlatform {
   /// Registers this class as the default instance of [OnAudioQueryPlatform].
@@ -28,49 +23,34 @@ class OnAudioQueryPlugin extends OnAudioQueryPlatform {
     OnAudioQueryPlatform.instance = OnAudioQueryPlugin();
   }
 
-  // Query methods
-  // final AudiosQuery _audiosQuery = AudiosQuery();
-  // final AlbumsQuery _albumsQuery = AlbumsQuery();
-  // final ArtistsQuery _artistsQuery = ArtistsQuery();
-  // final GenresQuery _genresQuery = GenresQuery();
-  // final ArtworkQuery _artworkQuery = ArtworkQuery();
+  @override
+  Future<List<AudioModel>> queryAudios({MediaFilter? filter}) async {
+    throw UnimplementedError();
+  }
 
-  // @override
-  // Future<List<AudioModel>> queryAudios({MediaFilter? filter}) async {
-  //   return _audiosQuery.queryAudios(filter: filter);
-  // }
+  @override
+  Future<List<AlbumModel>> queryAlbums({MediaFilter? filter}) async {
+    throw UnimplementedError();
+  }
 
-  // @override
-  // Future<List<AlbumModel>> queryAlbums({MediaFilter? filter}) async {
-  //   return _albumsQuery.queryAlbums(
-  //     _audiosQuery.listOfAudios,
-  //     filter: filter,
-  //   );
-  // }
+  @override
+  Future<List<ArtistModel>> queryArtists({MediaFilter? filter}) async {
+    throw UnimplementedError();
+  }
 
-  // @override
-  // Future<List<ArtistModel>> queryArtists({MediaFilter? filter}) async {
-  //   return _artistsQuery.queryArtists(
-  //     _audiosQuery.listOfAudios,
-  //     filter: filter,
-  //   );
-  // }
+  @override
+  Future<List<GenreModel>> queryGenres({MediaFilter? filter}) async {
+    throw UnimplementedError();
+  }
 
-  // @override
-  // Future<List<GenreModel>> queryGenres({MediaFilter? filter}) async {
-  //   return _genresQuery.queryGenres(
-  //     filter: filter,
-  //   );
-  // }
-
-  // @override
-  // Future<Uint8List?> queryArtwork(
-  //   int id,
-  //   ArtworkType type, {
-  //   ArtworkFormat? format,
-  //   int? size,
-  //   int? quality,
-  // }) async {
-  //   return _artworkQuery.queryArtwork(id, type, format, size, quality);
-  // }
+  @override
+  Future<ArtworkModel> queryArtwork(
+    int id,
+    ArtworkType type, {
+    bool? fromAsset,
+    bool? fromAppDir,
+    MediaFilter? filter,
+  }) async {
+    throw UnimplementedError();
+  }
 }
